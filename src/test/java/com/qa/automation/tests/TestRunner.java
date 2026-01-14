7f68dafe3971514e8eaa4f6422728efad5d12cf6
Saurabh Sharma <saurabhsharma@Saurabhs-MacBook-Pro.local>
2026-01-15 00:58:37 +0530

    Add Extent Reports integration for Cucumber tests

--- src/test/java/com/qa/automation/tests/TestRunner.java
+++ src/test/java/com/qa/automation/tests/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "com.qa.automation.stepdefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html"}
+    plugin = {"pretty", "html:target/cucumber-reports.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
