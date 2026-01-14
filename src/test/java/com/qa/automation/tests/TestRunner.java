80c4b0cb3d9dee85ae0c151e82bbffc93cb7618c
Saurabh Sharma <saurabhsharma@Saurabhs-MacBook-Pro.local>
2026-01-15 01:02:11 +0530

    Add detailed Cucumber reports (JSON, XML, HTML)

--- src/test/java/com/qa/automation/tests/TestRunner.java
+++ src/test/java/com/qa/automation/tests/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "com.qa.automation.stepdefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json", "junit:target/cucumber.xml", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
