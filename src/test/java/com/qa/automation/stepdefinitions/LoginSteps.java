496ab9f8bdc650db4a150172a12a72f5fed28ec4
Saurabh Sharma <saurabhsharma@Saurabhs-MacBook-Pro.local>
2026-01-15 01:48:45 +0530

    Implement Page Object Model (POM) with locators enum and page classes

--- src/test/java/com/qa/automation/stepdefinitions/LoginSteps.java
+++ src/test/java/com/qa/automation/stepdefinitions/LoginSteps.java
@@ -1,22 +1,22 @@
 package com.qa.automation.stepdefinitions;
 
 import com.qa.automation.base.BaseTest;
+import com.qa.automation.pages.GoogleHomePage;
 import com.qa.automation.utils.ConfigReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 public class LoginSteps extends BaseTest {
 
+    private GoogleHomePage googleHomePage;
+
     @Before
     public void setUp() {
         initializeDriver();
+        googleHomePage = new GoogleHomePage(driver, wait);
     }
 
     @After
@@ -27,14 +27,13 @@ public class LoginSteps extends BaseTest {
     @Given("the user is on the Google homepage")
     public void theUserIsOnTheGoogleHomepage() {
         String url = ConfigReader.getUrl();
-        driver.get(url);
+        googleHomePage.navigateTo(url);
     }
 
     @Then("the page title should be {string}")
     public void thePageTitleShouldBe(String expectedTitle) {
-        // Example of using explicit wait
-        wait.until(ExpectedConditions.titleIs(expectedTitle));
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(actualTitle, expectedTitle);
+        String actualTitle = googleHomePage.getGooglePageTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle), 
+            "Expected title to contain: " + expectedTitle + ", but got: " + actualTitle);
     }
 }
